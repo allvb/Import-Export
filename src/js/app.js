99a1fa86d745bda5240d23fb5547a908b01bef42
All <allvb@rambler.ru>
2023-03-27 21:46:26 +0300

    Вторая задача

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import { GameSavingData, readGameSaving, writeGameSaving } from "./game";
+import Game { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from "./game";
+
+const game = new Game();
+game.start();
